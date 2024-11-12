cde80ae2943f66d4ef483e5cbf2aaf4608f3937c
andrew Koller <avkoll>
2024-11-12 00:13:32 -0500

    refactored

--- lorchonlinewebsite/vite.config.js
+++ lorchonlinewebsite/vite.config.js
@@ -1,17 +1,16 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vuetify from 'vite-plugin-vuetify';
 
 export default defineConfig({
+  root: './lorchonlinewebsite',  // Set the root to the frontend directory (corrected syntax)
   plugins: [
     vue(),
     vuetify({ autoImport: true }), // enables Vuetify components and directives
   ],
   build: {
-    outDir: 'dist',
+    outDir: '../dist',  // Updated to place the output in the 'dist' folder at the root level
     emptyOutDir: true,
   },
-  root: 'frontend',
-  base: '/lorchwebsite/',
+  base: '/lorchwebsite/',  // Correct base URL for deployment
 });
